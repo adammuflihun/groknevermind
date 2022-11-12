bc66f2596029b4142a950fae75830696f2c6ba26
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-11-12 14:49:54 +0700

    s

--- src/utils/gsapHome.ts
+++ src/utils/gsapHome.ts
@@ -5,4 +5,13 @@ gsap.registerPlugin(ScrollToPlugin);
 
 export const gsapHome = () => {
   console.log('ancolkoncol');
+  gsap.to('.grid-block-background', {
+    backgroundColor: '#222222',
+    stagger: {
+      each: 0.1,
+      from: 'random',
+      ease: 'power2.inOut',
+      repeat: -1,
+    },
+  });
 };
