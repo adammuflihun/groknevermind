b6d6873d26cb9fac08c402c18646e67686fdf9f2
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-11-12 14:31:40 +0700

    s

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import { greetUser } from '$utils/greet';
+// import { greetUser } from '$utils/greet';
+import { gsapHome } from '$utils/gsapHome';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
   const name = 'John Doe';
   greetUser(name);
 });
+
+gsapHome();
---
+++ src/utils/gsapHome.ts
@@ -0,0 +1,8 @@
+import { gsap } from 'gsap';
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
+
+gsap.registerPlugin(ScrollToPlugin);
+
+export const gsapHome = () => {
+  console.log('ancolkoncol');
+};
