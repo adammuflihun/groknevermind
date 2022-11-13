71ad6d623b0efba6b220273db4f3d1170d8b0753
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-11-13 15:16:13 +0700

    s

--- src/utils/splitText.ts
+++ src/utils/splitText.ts
@@ -3,7 +3,7 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import SplitType from 'split-type';
 
 gsap.registerPlugin(ScrollTrigger);
-gsap.set(' .title-2 ,  .title-4  , .title5 , .title-6 , .title-7 ', {
+gsap.set(' .title-2 ,  .title-3,  .title-4  , .title5 , .title-6 , .title-7 ', {
   opacity: 0,
 });
 
@@ -30,16 +30,7 @@ export const SplitTextHome = () => {
           trigger: '.trigger-0',
           start: 'top 1rem',
           end: 'top 1rem',
-          // markers: true,
-          onEnterBack: () => {
-            gsap.to($('.title-2').find('.word'), 0.1, {
-              y: '0rem',
-              opacity: 0,
-              duration: 0.1,
-              stagger: 0.1,
-              ease: 'expo.out',
-            });
-          },
+
           toggleActions: 'play none reverse none',
         },
         y: '2rem',
@@ -85,7 +76,7 @@ export const SplitTextHome = () => {
             end: 'top 2rem',
             // markers: true,
 
-            toggleActions: 'play restart reset restart',
+            toggleActions: 'play none reverse none',
           },
           y: '0rem',
           opacity: 1,
@@ -117,7 +108,7 @@ export const SplitTextHome = () => {
           trigger: '.trigger-2',
           start: 'top 0%',
           end: 'top 2rem%',
-          toggleActions: 'play none reverse none',
+          toggleActions: 'play none none none',
         },
         opacity: 1,
       });
@@ -129,15 +120,7 @@ export const SplitTextHome = () => {
             trigger: '.trigger-2',
             start: 'top 0%',
             end: 'top 2rem%',
-            onEnter: () => {
-              gsap.to($('.title-2').find('.word'), {
-                y: '2rem',
-                opacity: 0,
-                duration: 0.2,
-                stagger: 0.1,
-                ease: 'expo.out',
-              });
-            },
+
             toggleActions: 'play none reverse none',
           },
           y: '0rem',
@@ -151,6 +134,57 @@ export const SplitTextHome = () => {
   };
 
   // animation 4
+
+  let addAnimation4 = function () {
+    $('.title-4').each(function (index) {
+      const text = new SplitType($(this), {
+        types: 'lines, words',
+        lineClass: 'word-line',
+      });
+
+      let textInstance = $(this);
+      let line = textInstance.find('.word-line');
+      let word = $('.title-4').find('.word');
+      let tl = gsap.timeline({});
+
+      // tl.set(textInstance, { opacity: 0 });
+      gsap.to(textInstance, 0.05, {
+        scrollTrigger: {
+          trigger: '.trigger-3',
+          start: 'top 0%',
+          end: 'top 2rem%',
+          toggleActions: 'play none none none',
+        },
+        opacity: 1,
+      });
+      gsap.fromTo(
+        word,
+        { opacity: 0, y: '2rem' },
+        {
+          scrollTrigger: {
+            trigger: '.trigger-3',
+            start: 'top 0%',
+            end: 'top 2rem%',
+            // onEnter: () => {
+            //   gsap.to($('.title-3').find('.word'), {
+            //     y: '2rem',
+            //     opacity: 0,
+            //     duration: 0.2,
+            //     stagger: 0.1,
+            //     ease: 'expo.out',
+            //   });
+            // },
+            toggleActions: 'play none reverse none',
+          },
+          y: '0rem',
+          opacity: 1,
+          duration: 0.2,
+          stagger: 0.1,
+          ease: 'expo.out',
+        }
+      );
+    });
+  };
 
   //restart animation
   addAnimation();
