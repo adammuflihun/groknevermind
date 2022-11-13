8be697d118564cf1725689dce0cab0049328e64b
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-11-13 13:17:59 +0700

    s

--- src/utils/splitText.ts
+++ src/utils/splitText.ts
@@ -3,7 +3,7 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import SplitType from 'split-type';
 
 gsap.registerPlugin(ScrollTrigger);
-gsap.set(' .title-3 , .title-4 , .title5, .title-6, .title-7', { opacity: 0 });
+gsap.set('   .title-4 , .title5, .title-6, .title-7', { opacity: 0 });
 
 export const SplitTextHome = () => {
   let windowWidth = window.outerWidth;
@@ -91,6 +91,66 @@ export const SplitTextHome = () => {
           opacity: 1,
           duration: 0.2,
           stagger: 0.1,
+          ease: 'expo.out',
+        }
+      );
+      gsap.to(word, {
+        scrollTrigger: {
+          trigger: '.trigger-1out',
+          start: 'top 0%',
+          end: 'top 2rem%',
+          // markers: true,
+          toggleActions: 'play none reverse none',
+        },
+        y: '2rem',
+        opacity: 0,
+        duration: 0.2,
+        stagger: 0.1,
+        ease: 'expo.out',
+      });
+    });
+  };
+
+  // animation 3
+
+  let addAnimation3 = function () {
+    $('.title-3').each(function (index) {
+      const text = new SplitType($(this), {
+        types: 'lines, words',
+        lineClass: 'word-line',
+      });
+
+      let textInstance = $(this);
+      let line = textInstance.find('.word-line');
+      let word = $('.title-3').find('.word');
+      let tl = gsap.timeline({});
+
+      // tl.set(textInstance, { opacity: 0 });
+      gsap.to(textInstance, 0.05, {
+        scrollTrigger: {
+          trigger: '.trigger-2',
+          start: 'top 0%',
+          end: 'top 2rem%',
+          // markers: true,
+          toggleActions: 'play none reverse none',
+        },
+        opacity: 1,
+      });
+      gsap.fromTo(
+        word,
+        { opacity: 0, y: '2rem' },
+        {
+          scrollTrigger: {
+            trigger: '.trigger-2',
+            start: 'top 0%',
+            end: 'top 2rem%',
+            // markers: true,
+            toggleActions: 'play none reverse none',
+          },
+          y: '0rem',
+          opacity: 1,
+          duration: 0.2,
+          stagger: 0.1,
           // ease: 'expo.out',
         }
       );
@@ -100,6 +160,7 @@ export const SplitTextHome = () => {
   //restart animation
   addAnimation();
   addAnimation2();
+  addAnimation3();
 
   window.addEventListener('resize', function (event) {
     addAnimation();
