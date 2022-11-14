660e7e0b960f6a63c21d730e2f028a6034bcfab0
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-11-14 14:36:30 +0700

    s

--- src/utils/gsapHome.ts
+++ src/utils/gsapHome.ts
@@ -5,34 +5,34 @@ gsap.registerPlugin(ScrollToPlugin);
 
 export const gsapHome = () => {
   console.log('ancolkoncol');
-  const colorBg1 = gsap.to('.grid-block-background', 0.5, {
-    backgroundColor: '#222222',
-    repeat: -1,
+  // const colorBg1 = gsap.to('.grid-block-background', 0.5, {
+  //   backgroundColor: '#222222',
+  //   repeat: -1,
 
-    stagger: {
-      each: 1,
-      from: 'random',
-    },
-  });
-  const colorBg2 = gsap.to('.grid-block-background', 1, {
-    backgroundColor: '#171717',
-    repeat: -1,
+  //   stagger: {
+  //     each: 1,
+  //     from: 'random',
+  //   },
+  // });
+  // const colorBg2 = gsap.to('.grid-block-background', 1, {
+  //   backgroundColor: '#171717',
+  //   repeat: -1,
 
-    stagger: {
-      each: 0.5,
-      from: 'random',
-    },
-  });
+  //   stagger: {
+  //     each: 0.5,
+  //     from: 'random',
+  //   },
+  // });
 
-  const colorBg = gsap.to('.grid-block-background', 0.6, {
-    backgroundColor: '#FFB82D',
-    repeat: -1,
+  // const colorBg = gsap.to('.grid-block-background', 0.6, {
+  //   backgroundColor: '#FFB82D',
+  //   repeat: -1,
 
-    stagger: {
-      each: 2,
-      from: 'random',
-    },
-  });
+  //   stagger: {
+  //     each: 2,
+  //     from: 'random',
+  //   },
+  // });
 
   let scrollBottom = function () {
     gsap.to('.grid-block-background, .wrapper-content-grid, .center-content', {
@@ -42,16 +42,16 @@ export const gsapHome = () => {
         end: '0% 100%',
         // markers: true,
         toggleActions: 'play none reverse none',
-        onEnter: () => {
-          colorBg.kill();
-          colorBg1.kill();
-          colorBg2.kill();
-        },
-        onLeaveBack: () => {
-          colorBg.restart();
-          colorBg1.restart();
-          colorBg2.restart();
-        },
+        // onEnter: () => {
+        //   colorBg.kill();
+        //   colorBg1.kill();
+        //   colorBg2.kill();
+        // },
+        // onLeaveBack: () => {
+        //   colorBg.restart();
+        //   colorBg1.restart();
+        //   colorBg2.restart();
+        // },
       },
       // borderWidth: '0',
       // autoAlpha: 0,
